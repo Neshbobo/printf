0293c0cf01fa79847ac5f25bb235f7d26640e1ae
Neshbobo <138696608+Neshbobo@users.noreply.github.com>
2023-11-17 21:54:26 +0300

    Update 1_Education_is_when_you_read_the_fine_print.c
    partner project

--- 1_Education_is_when_you_read_the_fine_print.c
+++ 1_Education_is_when_you_read_the_fine_print.c
@@ -1,71 +1,76 @@
-/**
- *  _printf.cpp
- *
- *  Created on: Nov 15, 2023
- *  Author: Neema and Yatich
- */
-
 #include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
-/*
- * _printf: Prints formatted output to the standard output stream.
+/**
+ * _printf - Custom printf function supporting %d and %i
+ * @format: The format string containing directives
  *
- * Parameters:
- * format: The format string to use for formatting the output.
- * ...: The arguments to be formatted according to the format string.
- *
- * Returns:
- * The number of characters printed (excluding the null byte used to end output to strings).
+ * Return: The number of characters printed (excluding the null byte)
  */
-
-
-int _printf(const char *format, ...) 
+int _printf(const char *format, ...)
 {
-int count = 0;  // Counter for the number of characters printed
-va_list args;  // Variable-argument list for accessing the arguments
-va_start(args, format);  // Initialize the variable-argument list
+    va_list args;
+    int count = 0;
 
-while (*format != '\0') 
-{  
-if (*format == '%') 
-{  
-switch (*++format) 
-{  
-case 'c':  
-count += putchar(va_arg(args, int));  // Print the character argument
-break;
+    va_start(args, format);
 
-case 's':  // Handle string format specifier
-count += puts(va_arg(args, const char *));  // Print the string argument
-break;
+    while (*format)
+    {
+        if (*format == '%' && *(format + 1) != '\0')
+        {
+            format++; /* Move past the % character */
 
-case '%':  // Handle literal percent sign
-count += putchar('%');  // Print the percent sign
-break;
+            switch (*format)
+            {
+                case 'c':
+                    count += write(1, &va_arg(args, int), 1);
+                    break;
 
-case 'd':  // Handle signed decimal integer format specifier
-count += print_decimal(va_arg(args, int));  // Print the integer as a decimal number
-break;
+                case 's':
+                    count += write(1, va_arg(args, char *), 1);
+                    break;
 
-case 'i':  // Handle signed decimal integer format specifier (equivalent to 'd')
-count += print_decimal(va_arg(args, int));  // Print the integer as a decimal number
-break;
+                case 'd':
+                case 'i':
+                    {
+                        int num = va_arg(args, int);
+                        char buffer[12]; /* Assumes a 32-bit integer max length */
+                        int length = sprintf(buffer, "%d", num);
+                        count += write(1, buffer, length);
+                    }
+                    break;
 
-default:  // Unrecognized format specifier
-count += putchar('%');  // Print the percent sign
-count += putchar(*format);  // Print the unrecognized character
-break;
+                case '%':
+                    count += write(1, "%", 1);
+                    break;
+
+                default:
+                    count += write(1, "%", 1);
+                    count += write(1, format, 1);
+                    break;
+            }
+        }
+        else
+        {
+            count += write(1, format, 1);
+        }
+
+        format++;
+    }
+
+    va_end(args);
+
+    return (count);
 }
-} 
-else 
-{  
-count += putchar(*format);  // Print the literal character
-}
 
-++format;  // Advance to the next character in the format string
-}
-
-va_end(args);  // Clean up the variable-argument list
-return (count); // Return the number of characters printed
+/**
+ * main - Example usage of _printf
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("Hello, %c! This is a %s example. The value of %d is %i.\n", 'W', "printf", 10, -20);
+    return (0);
 }
